61f1ee071be5459e131aa9655607f90dc6df122f
Alvin Ngow <alvin@lcclcoding.com>
2022-04-03 19:10:24 +0800

    reddit popular mental illness

--- scrapper.py
+++ scrapper.py
@@ -1,25 +1,35 @@
+import matplotlib.pyplot as plt
 import praw
 import pandas as pd
 import numpy as np
 import advertools as adv
 from prawcore.exceptions import Forbidden
 
+
+conditions = ["depression", "ptsd", "anxiety", "bipolar",
+              "schizophrenia", "ocd", "paranoia", "anorexia", "psychosis"]
+lenArray = []
+
+noOfPosts = []
+total = 0
+allPost = pd.DataFrame()
+
+
 # api
 reddit = praw.Reddit(client_id='EQlR29dhRQlv4Y31BZspAA',
                      client_secret='ae7DSXt0nDCYzqsWuhkVTrlCVJLq1g', user_agent='school-related')
 
-conditions = ["depression", "ptsd", "anxiety"]
-# , "bipolar","schizophrenia", "ocd", "paranoia", "anorexia", "psychosis"]
 
 params = {'sort': 'new', 'limit': None, 'syntax': 'cloudsearch'}
-# posts with "mental health" in title in r/Singapore
 
-allPost = pd.DataFrame()
-noOfPosts = []
+
+print("retrieving")
 
 # retrieve information about posts
 for condition in conditions:
+    print(condition)
     posts = []
+    # search for post
     posts_about_mental_health = reddit.subreddit(
         "Singapore").search(condition, **params)
 
@@ -30,7 +40,13 @@ for condition in conditions:
     posts = pd.DataFrame(posts, columns=[
         'title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created', 'author'])
 
-    noOfPosts.append({condition: len(posts)})
+    length = len(posts)
+    noOfPosts.append({condition: length})
+    lenArray.append(length)
+    total += len(posts)
     # print(posts_about_mental_health)
 
-# print(noOfPosts)
+x = np.array(lenArray)
+plt.pie(x, labels=conditions)
+plt.legend(conditions, bbox_to_anchor=(0, 1), loc='upper left', ncol=1)
+plt.show()
